b0dd63dcae3cc6c4f487b6ca25f8d643b817a667
Novey Am <noveycea@gmail.com>
2023-05-17 16:41:26 -0700

    arranged navbar component

---
+++ bday-app/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav>
+            <figure id="pokeball">
+                <img id="pokeball" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg" alt="half red half white ball"></img>
+            </figure>
+            <ul className="navbar">
+                <li>Home</li>
+                <li>About</li>
+                <li>Reservations</li>
+                <li>Contact</li>
+            </ul>
+        </ nav>
+    )
+}
+
+export default NavBar;
