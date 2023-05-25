76867adc14321bfce30a4309a433bd1d64588610
Novey Am <noveycea@gmail.com>
2023-05-25 15:46:30 -0700

    remove navbar.js to app.js

--- bday-app/src/App.js
+++ bday-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from './components/NavBar';
 import Main from './components/Main';
 import Form from './components/Form';
 import './App.css';
@@ -7,7 +6,17 @@ import './App.css';
 function App() {
   return (
     <>
-    <NavBar />
+    <nav>
+      <figure id="pokeball">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg" alt="half red half white ball"></img>
+      </figure>
+        <ul className="navbar">
+          <li>Home</li>
+          <li>About</li>
+          <li>Reservations</li>
+          <li>Contact</li>
+        </ul>
+    </ nav>
     <Main />
     <Form />
     </>
